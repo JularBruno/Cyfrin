ecf2fc2b7bc6bd968d9e0952b99927c4324740da
Bruno Jular <brunojular00@gmail.com>
2025-01-24 00:02:40 -0300

    up to 23

--- Solidity Smart Contract Development/section3/FundMe.sol
+++ Solidity Smart Contract Development/section3/FundMe.sol
@@ -6,12 +6,18 @@
 
 pragma solidity 0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; // interfaces are for interact with contract without actual function
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; // interfaces are for interact with ABI contract without actual functions in it
+
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
 
+    using PriceConverter for uint256; // FOR LIBRARY PriceConverter, since it can be called directly on uint256
+
     // uint256 public myValue = 1; // this is used to explain revert
     uint256 public minimumUsd = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded; // map senders and the sent value
 
     function fund() public payable{
         // allow to send $
@@ -23,37 +29,21 @@ contract FundMe {
 
         // how to send eth to this contract?
         // require(msg.value >= minimumUsd, "didn't send enough eth"); // 1e18 = 1 eth = 1 * 10 ** 18 = valuew in wei (** means raised)
-        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough eth"); // require is kind of trycatch
+        
+        // require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough eth"); // require is kind of trycatch
+
+        // here is the version where a library is used. Can have parameters but msg.value will be always the first parameter
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough eth"); 
 
         // https breaks conscensus
         // with chainlink networks because of oracle problem
 
         // msg.value 18 decimal places because of Wei
+
+        funders.push(msg.sender); // another global variable that can be called in solidity for getting the sender, can be seen in docs
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     // function withdraw() public {}
 
-    function getPrice() public view returns (uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306 // eth usd sepolia
-        // ABI list of functions of a contract
-        // bboth things are required for this 
-
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        // prettier-ignore
-        (, int256 answer, , , )  = priceFeed.latestRoundData(); // int because values can be negative
-        // uint80 roundID, ,uint startedAt ,uint timeStamp ,uint80 answeredInRound
-
-        return uint256(answer * 1e10); // decimal places from eth to usd, and also from int to uint
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        // 1 ETH?
-        // 2000_0000000000000000000
-        uint256 ethPrice = getPrice();
-        // 1 / 2 = 0
-        // 2000_0000000000000000000 * 1_000000000000000000) / 1e18
-        // $2000 = 1 ETH
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
 }
